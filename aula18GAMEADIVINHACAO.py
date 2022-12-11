37cb98bc29e524fac4afce9839be7275221c91df
Sergio Donato <sergiodcdn@gmail.com>
2022-12-11 12:09:40 -0300

    Testo melhorado

--- aula18GAMEADIVINHACAO.py
+++ aula18GAMEADIVINHACAO.py
@@ -1,16 +1,17 @@
 import random
 import os
 
-erros=0
-sorteado=random.randrange(0,100)
-jogador=int(input("Digite seu numero: ")) #int pra chamar numero
+erros = 0
+sorteado = random.randrange(0, 100)
+# int pra chamar numero
+jogador = int(input("Adivinhe um numero entre 0 e 100: "))
 
-while(sorteado!=jogador):
+while (sorteado != jogador):
     os.system('cls')
-    if(sorteado>jogador):
+    if (sorteado > jogador):
         print("Erro, o numero sorteado eh maior")
-    elif(sorteado<jogador):
+    elif (sorteado < jogador):
         print("Erro, o numero eh menor")
-    erros+=1
-    jogador=int(input("Digite seu numero: "))
-print("Numero " + str(jogador) + ", voce acertou em " + str(erros+1) + "tentativas" )
+    erros += 1
+    jogador = int(input("Digite seu numero: "))
+print("Numero " + str(jogador) + ", voce acertou em " + str(erros+1) + "tentativas")
